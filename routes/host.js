7fca1436f1f3205110e30489a0841406480e1d6c
duy1711ak <duy.pham1711a@hcmut.edu.vn>
2022-05-21 10:05:40 +0700

    change api host/room

--- routes/host.js
+++ routes/host.js
@@ -158,18 +158,21 @@ router.delete('/:id/rooms/:rid', async (req, res) => {
         const hId = req.params.id;
         const roomId = parseInt(req.params.rid);
         var roomList = await roomModel.find({ hostId: hId});
-        await clientModel.findOneAndDelete({ roomId: roomId});
+        const clientInRoom = await clientModel.find({ roomId: roomId});
+        if (clientInRoom.length != 0) {
+            res.status(400).send({message: "Host does not exist"});
+        }
         const host = await usersModel.find({$and:[{"UId": hId},{"isClient": false}]});
         if (host.length == 0) {
-            res.status(400).send("Host does not exist");
+            res.status(400).send({message: "Host does not exist"});
         } else {
             if (roomList.length == 0) {
-                res.status(400).send("Room does not exist");
+                res.status(400).send({message: "Room does not exist"});
             } else {
                 roomList = roomList[0].roomList;
                 var existRoom = roomList.filter((x) => x.roomId == roomId);
                 if (existRoom.length == 0) {
-                    res.status(400).send("Room does not exist");
+                    res.status(400).send({message: "Room does not exist"});
                 } else {
                     var newRoomList = roomList.filter((x) => x.roomId != roomId);
                     newRoomList = await roomModel.findOneAndUpdate({ hostId: hId }, { roomList: newRoomList } , { new: true })
