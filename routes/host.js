fd0355d1bd4efa387a60aa07fe0e6f9b0753391a
duy1711ak <duy.pham1711a@hcmut.edu.vn>
2022-05-30 21:21:54 +0700

    chang api log in and register

--- routes/host.js
+++ routes/host.js
@@ -217,7 +217,7 @@ router.get('/:hid/notification/page/:pageNum', async (req, res) => {
                 );
             }
             else {
-                result = await list.slice(0, 3).reverse().map(
+                result = await list.slice(0, 3).map(
                     (obj)=>{
                         return {
                             "id" : obj.id,
